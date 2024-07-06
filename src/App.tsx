b05d17903b9a2b33eb02598d478d370990f6e723
Julia Pinheiro Oliveira <42902329+JuliaPinheiro@users.noreply.github.com>
2024-07-06 17:54:22 -0300

    feat: card

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,14 @@ import Box from '@mui/material/Box';
 import VideoSection from './components/videoSection';
 import Carousel from './components/carousel';
 import CarouselProduct from './components/productCard';
-import { Link, Element } from 'react-scroll';
 
 function App() {
   return (
     <Box>
       <Navbar />
-
-      <Element name='carousel'>
-        <Carousel />
-      </Element>
-      <Element name='videos'>
-        <VideoSection />
-      </Element>
-      <Element name='products'>
-        <CarouselProduct />
-      </Element>
+      <Carousel />
+      <VideoSection />
+      <CarouselProduct />
     </Box>
   );
 }
--- src/components/productCard/index.tsx
+++ src/components/productCard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import {
   IconButton,
   Typography,
@@ -10,25 +10,68 @@ import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import productsData from '../../../public/data.json';
 
-const Carousel = () => {
+const Product = ({
+  product,
+  isMobile,
+}: {
+  product: any;
+  isMobile: boolean;
+}) => (
+  <Box
+    role='button'
+    key={product.id}
+    sx={{
+      width: isMobile ? 300 : 200,
+      height: isMobile ? 280 : 240,
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 2,
+      margin: '0 10px',
+      transition: 'transform 0.3s ease-in-out',
+      '&:hover': {
+        transform: isMobile ? 'scale(1)' : 'scale(1.10)',
+        textDecoration: 'none',
+      },
+      cursor: 'pointer',
+    }}
+  >
+    <img
+      src={product.image_url}
+      alt={product.name}
+      style={{
+        width: '100%',
+        height: 'auto',
+        maxHeight: 128,
+        objectFit: 'contain',
+      }}
+    />
+    <Typography variant='body2'>{product.name}</Typography>
+    <Typography variant='body2' color='text.secondary'>
+      R$ {product.price}
+    </Typography>
+  </Box>
+);
+
+const CarouselProduct = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
   const productsToShow = isMobile ? 1 : 4;
 
-  const goToPrevious = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex - productsToShow >= 0 ? prevIndex - productsToShow : 0
-    );
-  };
+  const goToPrevious = useCallback(() => {
+    setCurrentIndex((prevIndex) => Math.max(prevIndex - productsToShow, 0));
+  }, [productsToShow]);
 
-  const goToNext = () => {
+  const goToNext = useCallback(() => {
     setCurrentIndex((prevIndex) =>
       prevIndex + productsToShow < productsData.length
         ? prevIndex + productsToShow
         : 0
     );
-  };
+  }, [productsToShow]);
+
   const visibleProducts = productsData.slice(
     currentIndex,
     currentIndex + productsToShow
@@ -43,47 +86,20 @@ const Carousel = () => {
         overflow: 'hidden',
       }}
     >
-      <IconButton onClick={goToPrevious} disabled={currentIndex === 0}>
+      <IconButton
+        onClick={goToPrevious}
+        disabled={currentIndex === 0}
+        aria-label='Previous'
+      >
         <ArrowBackIosNewIcon />
       </IconButton>
       {visibleProducts.map((product) => (
-        <Box
-          key={product.id}
-          sx={{
-            width: isMobile ? 300 : 200,
-            height: isMobile ? 280 : 240,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            padding: 2,
-            margin: '0 10px',
-            transition: 'transform 0.3s ease-in-out',
-            '&:hover': {
-              transform: isMobile ? 'scale(1)' : 'scale(1.10)',
-              textDecoration: 'none',
-            },
-          }}
-        >
-          <img
-            src={product.image_url}
-            alt={product.name}
-            style={{
-              width: '100%',
-              height: 'auto',
-              maxHeight: 128,
-              objectFit: 'contain',
-            }}
-          />
-          <Typography variant='body2'>{product.name}</Typography>
-          <Typography variant='body2' color='text.secondary'>
-            R$ {product.price}
-          </Typography>
-        </Box>
+        <Product key={product.id} product={product} isMobile={isMobile} />
       ))}
       <IconButton
         onClick={goToNext}
         disabled={currentIndex + productsToShow >= productsData.length}
+        aria-label='Next'
       >
         <ArrowForwardIosIcon />
       </IconButton>
@@ -91,4 +107,4 @@ const Carousel = () => {
   );
 };
 
-export default Carousel;
+export default CarouselProduct;
